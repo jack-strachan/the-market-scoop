093b7f85198314fc81b68b638b75bd9f8db30436
jack-strachan <jackstrachan@finary.io>
2022-04-18 20:05:28 -0400

    splash text improvement

--- src/sections/SplashTop.js
+++ src/sections/SplashTop.js
@@ -21,7 +21,7 @@ class SplashTop extends Component {
                             One Message,<br/> Per Day.
                         </h1>
                         <div className='mr-80'>
-                            <p className='o5 mt-16 mb-20' style={{fontSize: this.props.mobile ? 14 : 20}}>
+                            <p className='o5 mt-32 mb-8 uppercase' style={{fontSize: this.props.mobile ? 12 : 14, fontWeight:'bold'}}>
                                 Giving you the latest market updates, trending sectors, key events, and much more.
                             </p>
                             <p className='o5 mt-16 mb-20' style={{fontSize: this.props.mobile && 14}}>
